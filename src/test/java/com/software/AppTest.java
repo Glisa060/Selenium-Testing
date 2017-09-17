a19570d3ae7af4d88b823e75f800bb680446a617
Milan Glisic <milanglisic@ymail.com>
2017-09-17 15:31:53 +0200

    Update

--- src/test/java/com/software/AppTest.java
+++ src/test/java/com/software/AppTest.java
@@ -1,15 +1,13 @@
 package com.software;
 
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
-
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 
 /**
@@ -17,12 +15,12 @@ import java.util.List;
  */
 public class AppTest
 {
-//    private final String FILE = "C:\\Users\\Milan\\IdeaProjects\\Selenium Testing\\src\\chromedriver.exe";
-//    private final String DRIVER = "webdriver.chrome.driver";
+    private final String FILE = "C:\\Users\\Milan\\IdeaProjects\\Selenium Testing\\src\\chromedriver.exe";
+    private final String DRIVER = "webdriver.chrome.driver";
     @org.junit.Test
     public void test () {
-//        System.setProperty(DRIVER, FILE);
-        WebDriver driver = new FirefoxDriver();
+        System.setProperty(DRIVER, FILE);
+        WebDriver driver = new ChromeDriver();
         driver.navigate().to("https://www.rt.com/news/");
         driver.manage().window().maximize();
         driver.findElement(By.className("seach-form__input-text")).sendKeys("America");
@@ -41,8 +39,8 @@ public class AppTest
 
     @org.junit.Test
     public void test2() {
-//        System.setProperty("webdriver.chrome.driver", FILE);
-        WebDriver driver = new FirefoxDriver();
+        System.setProperty(DRIVER, FILE);
+        WebDriver driver = new ChromeDriver();
         driver.navigate().to("http://www.echoecho.com/htmlforms10.htm");
         driver.manage().window().maximize();
         List<WebElement> list = driver.findElements(By.name("radio1"));
@@ -62,14 +60,20 @@ public class AppTest
 
     @org.junit.Test
     public void test3() {
-//        System.setProperty(DRIVER,FILE);
-        WebDriver driver = new FirefoxDriver();
+        System.setProperty(DRIVER,FILE);
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.navigate().to("https://jqueryui.com/draggable/");
         driver.manage().window().maximize();
-        WebDriverWait wait = new WebDriverWait(driver, 5);
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//div[@id='content']")));
-        WebElement element = driver.findElement(By.xpath("//div[@id='content']"));
+        List<WebElement> list = driver.findElements(By.tagName("iframe"));
+        System.out.println("The number of frames is: " +list.size());
+        WebElement frame = driver.findElement(By.xpath("//*[@id=\"content\"]/iframe"));
+        driver.switchTo().frame(frame);
+        WebElement element = driver.findElement(By.xpath("//*[@id=\"draggable\"]"));
         System.out.println(element.getText());
+        driver.switchTo().defaultContent();
+        WebElement Pageheader = driver.findElement(By.xpath("//*[@id=\"content\"]/h1"));
+        System.out.println("Header text is: " +Pageheader.getText());
         driver.close();
         driver.quit();
     }
